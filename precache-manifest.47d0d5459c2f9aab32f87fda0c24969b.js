a1d5bfcdbac86941011947978d033711cb6b2416
bhubhanshu <bhanu11082000gurjar@gmail.com>
2020-10-19 08:48:51 +0530

    Updates

---
+++ precache-manifest.47d0d5459c2f9aab32f87fda0c24969b.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "929851654024699e4f7528db71dc2b27",
+    "url": "/deals/index.html"
+  },
+  {
+    "revision": "e177743d3af0fd613a5d",
+    "url": "/deals/static/css/2.f69748ee.chunk.css"
+  },
+  {
+    "revision": "1b0ab8bdacd117473161",
+    "url": "/deals/static/css/main.cf466f4b.chunk.css"
+  },
+  {
+    "revision": "e177743d3af0fd613a5d",
+    "url": "/deals/static/js/2.3f956433.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/deals/static/js/2.3f956433.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b0ab8bdacd117473161",
+    "url": "/deals/static/js/main.cb01e799.chunk.js"
+  },
+  {
+    "revision": "ef4b57df8f64b0455c42",
+    "url": "/deals/static/js/runtime-main.d1d9872e.js"
+  },
+  {
+    "revision": "b24396917a138794160b24ff04a71d80",
+    "url": "/deals/static/media/WorkSans-VariableFont_wght.b2439691.ttf"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/deals/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/deals/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/deals/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/deals/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/deals/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/deals/precache-manifest.47d0d5459c2f9aab32f87fda0c24969b.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/deals/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
